4db92e08218bca803af948fb9b438531442141fa
Hannah Sullivan <hs085315@ohio.edu>
2020-04-08 13:31:22 -0400

    Changes to Login

---
+++ dbo.AspNetUsers.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[AspNetUsers]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [email] NVARCHAR(50) NULL, 
+    [emailConfirmed] NVARCHAR(50) NULL, 
+    [passwordHash] NVARCHAR(50) NULL, 
+    [securityStamp] NVARCHAR(50) NULL, 
+    [phoneNumber] BIGINT NULL, 
+    [phoneNumberConfirmed] BIGINT NULL, 
+    [twoFactorEnabled] NVARCHAR(50) NULL, 
+    [lockOutEndDateUTC] DATETIME NULL, 
+    [lockOutEnabled] NVARCHAR(50) NULL, 
+    [accessFailedCount] NVARCHAR(50) NULL, 
+    [username] NVARCHAR(50) NULL
+)
