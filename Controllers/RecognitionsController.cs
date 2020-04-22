063ee031acf2300a5ba3d1aeed5ef346ae1b05fa
Matthew Donahue <md753815@ohio.edu>
2020-04-21 21:33:20 -0400

    email

--- Controllers/RecognitionsController.cs
+++ Controllers/RecognitionsController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MIS4200_CentricProject_Team12.DAL;
 using MIS4200_CentricProject_Team12.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace MIS4200_CentricProject_Team12.Controllers
 {
@@ -38,8 +39,36 @@ namespace MIS4200_CentricProject_Team12.Controllers
 
         // GET: Recognitions/Create
         public ActionResult Create()
+
         {
+            SmtpClient myClient = new SmtpClient();
+            // the following line has to contain the email address and password of someone
+            // authorized to use the email server (you will need a valid Ohio account/password
+            // for this to work)
+            myClient.Credentials = new NetworkCredential("AuthorizedUser", "UserPassword");
+            MailMessage myMessage = new MailMessage();
+            // the syntax here is email address, username (that will appear in the email)
+            MailAddress from = new MailAddress("md753815@ohio.edu", "SysAdmin");
+            myMessage.From = from;
+            myMessage.To.Add("matthewdonahue7@gmail.com"); // this should be replaced with model data
+                                                    // as shown at the end of this document
+            myMessage.Subject = "MVC Email test";
+            // the body of the email is hard coded here but could be dynamically created using data
+            // from the model- see the note at the end of this document
+            myMessage.Body = "This is the body of the mail message. This can be essentially any length, and could come ";
+            myMessage.Body += "from the database, a variable, the return of another method...";
+            try
+            {
+                myClient.Send(myMessage);
+                TempData["mailError"] = "";
+            }
+            catch (Exception ex)
+            {
+                // this captures an Exception and allows you to display the message in the View
+                TempData["mailError"] = ex.Message;
+            }
             return View();
+
         }
 
         // POST: Recognitions/Create
